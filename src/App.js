e1a8f5db4bd24291da2e63b8edc1d14f6a41fb09
Michael Ries <michael.ries@gmail.com>
2017-09-08 15:48:56 -0500

    add basic Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
 
 class App extends Component {
   render() {
@@ -10,9 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form />
       </div>
     );
   }
---
+++ src/Form.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+ render () {
+   return (
+     <form className="demoForm">
+       <h2>Sign up</h2>
+       <div className="form-group">
+         <label htmlFor="email">Email address</label>
+         <input type="email" className="form-control"
+           name="email" />
+       </div>
+       <div className="form-group">
+         <label htmlFor="password">Password</label>
+         <input type="password" className="form-control"
+           name="password" />
+       </div>
+       <button type="submit" className="btn btn-primary">
+          Sign up
+       </button>
+     </form>
+   )
+ }
+}
+export default Form;
