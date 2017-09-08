5235bec5c8040f92c0d281285c601da58f8617b3
Michael Ries <michael.ries@gmail.com>
2017-09-08 16:17:15 -0500

    add bootstrap css links

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
