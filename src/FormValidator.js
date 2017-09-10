89d45bfdbbfca4a56856dc13b398553c4fe7b860
Michael Ries <michael.ries@gmail.com>
2017-09-10 11:24:34 -0500

    add extra comments for clarity

--- src/FormValidator.js
+++ src/FormValidator.js
@@ -2,23 +2,35 @@ import validator from 'validator';
 
 class FormValidator {
   constructor(validations) {
-
     // validations is an array of validation rules specific to a form
     this.validations = validations;
   }
 
   validate(state) {
+    // start out assuming valid
     let validation = this.valid();
-    this.validations.forEach(v => {
-      if (!validation[v.field].isInvalid) {
-        const args = v.args || [];
+
+    // for each validation rule
+    this.validations.forEach(rule => {
+
+      // if the field hasn't already been marked invalid by an earlier rule
+      if (!validation[rule.field].isInvalid) {
+        // determine the field value, the method to invoke and optional args from 
+        // the rule definition
+        const field_value = state[rule.field].toString();
+        const args = rule.args || [];
         const validation_method = 
-              typeof v.method === 'string' ?
-              validator[v.method] : 
-              v.method
+              typeof rule.method === 'string' ?
+              validator[rule.method] : 
+              rule.method
               
-        if(validation_method(state[v.field].toString(), ...args, state) !== v.validWhen) {
-          validation[v.field] = { isInvalid: true, message: v.message }
+        // call the validation_method with the current field value as the first
+        // argument, any additional arguments, and the whole state as a final
+        // argument.  If the result doesn't match the rule.validWhen property,
+        // then modify the validation object for the field and set the isValid
+        // field to false
+        if(validation_method(field_value, ...args, state) !== rule.validWhen) {
+          validation[rule.field] = { isInvalid: true, message: rule.message }
           validation.isValid = false;
         }
       }
@@ -30,8 +42,8 @@ class FormValidator {
   valid() {
     const validation = {}
 
-    this.validations.map(v => (
-      validation[v.field] = { isInvalid: false, message: '' }
+    this.validations.map(rule => (
+      validation[rule.field] = { isInvalid: false, message: '' }
     ));
 
     return { isValid: true, ...validation };
